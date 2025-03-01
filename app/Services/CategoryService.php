34ce19fc950453c89432adc3259593922acbd1f8
Pony <pony@ponycool.com>
2025-03-01 16:31:04 +0800

    Add navigation base class

---
+++ app/Services/CategoryService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pony
+ * Date: 2025/03/01
+ * Time: 16:30 下午
+ */
+declare(strict_types=1);
+
+namespace App\Services;
+
+class CategoryService extends BaseService
+{
+
+}
