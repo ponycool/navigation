7b871773986647feaffa5f1d6ef0e2eed3c3d9b6
Pony <pony@ponycool.com>
2025-03-04 17:16:45 +0800

    Create Disable.php

---
+++ app/Controllers/Api/Category/Disable.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created By PhpStorm
+ * User: Pony
+ * Data: 2025/3/4
+ * Time: 17:07
+ */
+declare(strict_types=1);
+
+namespace App\Controllers\Api\Category;
+
+use App\Controllers\Api\Base;
+use App\Enums\Code;
+use App\Services\CategoryService;
+use Exception;
+
+class Disable extends Base
+{
+    /**
+     * 禁用分类
+     * @return void
+     */
+    public function index(): void
+    {
+        $this->postFilter();
+        try {
+            $params = $this->getJsonInputParams();
+            $uuid = $params['uuid'] ?? null;
+            if ($this->validateUUID($uuid) !== true) {
+                throw new Exception('无效的分类UUID');
+            }
+            $svc = new CategoryService();
+            $data = [
+                'status' => 0
+            ];
+            $cond = [
+                'uuid' => $uuid
+            ];
+            $res = $svc->updateByCond($data, $cond);
+            if ($res === false) {
+                throw new Exception('禁用分类失败');
+            }
+            $data = [
+                'code' => Code::OK,
+                'message' => '禁用分类成功',
+            ];
+        } catch (Exception $e) {
+            $data = [
+                'code' => Code::FAIL,
+                'message' => $e->getMessage() ?: '禁用分类失败'
+            ];
+        }
+        $this->render($data);
+    }
+}
