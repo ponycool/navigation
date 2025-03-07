4113257c3eb4272503a2cfebb18ebcf02062dba4
Pony <pony@ponycool.com>
2025-03-07 16:06:39 +0800

    feat:  add  navigation record base class

---
+++ app/Services/RecordService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pony
+ * Date: 2025/03/07
+ * Time: 16:05 下午
+ */
+declare(strict_types=1);
+
+namespace App\Services;
+
+class RecordService extends BaseService
+{
+
+}
