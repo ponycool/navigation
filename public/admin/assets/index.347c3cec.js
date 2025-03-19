328ba7d34b9825892416bd36288cbf7aa79533e4
Pony <pony@ponycool.com>
2025-03-19 16:33:28 +0800

    feat: update admin ui

--- public/admin/assets/index.347c3cec.js
+++
@@ -1,1 +0,0 @@
-import{a as r,j as t,aU as i}from"./index.30acaf99.js";import{s as l,M as m}from"./item.3c80ff3f.js";function n(e){const{data:s=[]}=e;return r("div",{className:l["message-list"],children:[s.map(a=>t(m,{data:a},a.id)),!s.length&&t(i,{status:"404"})]})}export{n as default};
