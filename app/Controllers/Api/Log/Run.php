c570da161173498d97aec330b046e710820f5e57
Pony <pony@ponycool.com>
2025-03-25 16:03:05 +0800

    feat: add log api

---
+++ app/Controllers/Api/Log/Run.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Created By PhpStorm
+ * User: Pony
+ * Data: 2025/3/25
+ * Time: 15:38
+ */
+declare(strict_types=1);
+
+namespace App\Controllers\Api\Log;
+
+use App\Controllers\Api\Base;
+use App\Enums\Code;
+use App\Services\RunLogService;
+use CodeIgniter\Files\File;
+use PonyCool\File\File as FileUtil;
+use Exception;
+
+class Run extends Base
+{
+    /**
+     * 获取系统日志
+     * @return void
+     */
+    public function index(): void
+    {
+        $svc = new RunLogService();
+        $rules = $svc->getBaseRules();
+        $this->verifyJsonInputByRules($rules);
+        try {
+            $data = [
+                'code' => Code::OK,
+                'message' => '获取系统运行日志成功'
+            ];
+            if (!file_exists($svc->getLogFile())) {
+                throw new Exception('系统运行日志不存在');
+            }
+            $line = (int)($this->getJsonInputParam('line') ?? 50);
+            $fileUtil = new FileUtil();
+            $content = $fileUtil::getFileLastLines($svc->getLogFile(), $line);
+            $data = array_merge($data, $content);
+        } catch (Exception $e) {
+            $data = [
+                'code' => Code::FAIL,
+                'message' => $e->getMessage() ?: '获取系统运行日志失败',
+            ];
+        }
+        $this->render($data);
+    }
+
+    /**
+     * 下载运行日志
+     * @return void
+     */
+    public function download(): void
+    {
+        $svc = new RunLogService();
+        $logFile = $svc->getLogFile();
+        try {
+            if (!file_exists($logFile)) {
+                throw new Exception('系统运行日志不存在');
+            }
+
+            $file = new File($logFile);
+            $binary = readfile($logFile);
+            $this->response
+                ->setHeader('Content-Type', $file->getMimeType())
+                ->setHeader('Content-disposition', 'attachment; filename="' . $file->getBasename() . '"')
+                ->setStatusCode(200)
+                ->setBody($binary)
+                ->download();
+        } catch (Exception $e) {
+            $data = [
+                'code' => Code::FAIL,
+                'message' => $e->getMessage() ?: '下载系统运行日志失败',
+            ];
+            $this->render($data);
+        }
+    }
+
+    /**
+     * 清空运行日志
+     * @return void
+     */
+    public function clear(): void
+    {
+        try {
+            $data = [
+                'code' => Code::OK,
+                'message' => '清空系统运行日志成功'
+            ];
+            $svc = new RunLogService();
+            if (!file_exists($svc->getLogFile())) {
+                throw new Exception('系统运行日志不存在');
+            }
+            $res = $svc->clear();
+            if ($res !== true) {
+                throw new Exception('清空系统运行日志失败');
+            }
+        } catch (Exception $e) {
+            $data = [
+                'code' => Code::FAIL,
+                'message' => $e->getMessage() ?: '清空系统运行日志失败',
+            ];
+        }
+        $this->render($data);
+    }
+}
---
+++ app/Services/RunLogService.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created By PhpStorm
+ * User: Pony
+ * Data: 2025/3/25
+ * Time: 15:51
+ */
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Enums\LogCategory;
+use Carbon\Carbon;
+
+class RunLogService extends BaseService
+{
+    /**
+     * 获取基础验证规则
+     * @return array
+     */
+    public function getBaseRules(): array
+    {
+        return [
+            'line' => [
+                'rules' => 'if_exist|is_natural_no_zero',
+                'errors' => [
+                    'is_natural_no_zero' => '参数日志行数[line]无效，必须为非零自然数',
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * 清空运行日志
+     * @return bool
+     */
+    public function clear(): bool
+    {
+        $logFile = $this->getLogFile();
+        if (!file_exists($logFile)) {
+            return false;
+        }
+        if (is_dir($logFile)) {
+            log_message('warning', '清空运行日志失败，运行日志不是一个文件');
+            return false;
+        }
+        if (!is_writable($logFile)) {
+            log_message('warning', '清空运行日志失败，没有写入权限');
+            return false;
+        }
+
+        $logSvc = new LogService();
+        if (file_put_contents($logFile, "") !== false) {
+            $logSvc->info('清空运行日志', LogCategory::USER);
+        } else {
+            log_message(
+                'warning',
+                '清空运行日志失败',
+            );
+            $logSvc->warn('清空运行日志失败', LogCategory::USER);
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 获取日志文件
+     * @return string
+     */
+    public function getLogFile(): string
+    {
+        $date = Carbon::now()->toDateString();
+        return WRITEPATH . 'logs/log-' . $date . '.log';
+    }
+}
