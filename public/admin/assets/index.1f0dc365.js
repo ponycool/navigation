965917f82cae20a84f4c044425ffe2d1d95f6c5e
Pony <pony@ponycool.com>
2025-09-19 10:47:27 +0800

    feat: update admin ui

---
+++ public/admin/assets/index.1f0dc365.js
@@ -0,0 +1,1 @@
+import{a as r,j as t,aU as i}from"./index.d6a4ac8c.js";import{s as l,M as m}from"./item.d1a6e5fc.js";function n(e){const{data:s=[]}=e;return r("div",{className:l["message-list"],children:[s.map(a=>t(m,{data:a},a.id)),!s.length&&t(i,{status:"404"})]})}export{n as default};
--- public/admin/assets/index.6fa2cc0c.js
+++
@@ -1,1 +0,0 @@
-import{a as r,j as t,aU as i}from"./index.79bb1216.js";import{s as l,M as m}from"./item.00113722.js";function n(e){const{data:s=[]}=e;return r("div",{className:l["message-list"],children:[s.map(a=>t(m,{data:a},a.id)),!s.length&&t(i,{status:"404"})]})}export{n as default};
