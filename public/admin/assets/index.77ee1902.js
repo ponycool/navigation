8aa59c463d3a7ddbed674c749a505eec75f5e433
Pony <pony@ponycool.com>
2025-09-29 16:50:51 +0800

    feat: update admin ui

---
+++ public/admin/assets/index.77ee1902.js
@@ -0,0 +1,1 @@
+import{a as r,j as t,b8 as i}from"./index.6bfcbf90.js";import{s as l,M as m}from"./item.df2a3860.js";function n(e){const{data:s=[]}=e;return r("div",{className:l["message-list"],children:[s.map(a=>t(m,{data:a},a.id)),!s.length&&t(i,{status:"404"})]})}export{n as default};
