dea223b08c1d7c6719d5ffabddde299ad1249d8b
Pony <pony@ponycool.com>
2025-11-25 16:40:29 +0800

    Update admin asset bundles and index.html
    Renamed and updated multiple JavaScript asset files in public/admin/assets to reference new chunk names and dependencies. Also modified public/admin/index.html, likely to reflect updated asset references. These changes help ensure the admin panel loads the latest compiled assets after a build.

---
+++ public/admin/assets/index.dad146f1.js
@@ -0,0 +1,1 @@
+import{a as r,j as t,b8 as i}from"./index.42860b0d.js";import{s as l,M as m}from"./item.70d54c65.js";function n(e){const{data:s=[]}=e;return r("div",{className:l["message-list"],children:[s.map(a=>t(m,{data:a},a.id)),!s.length&&t(i,{status:"404"})]})}export{n as default};
