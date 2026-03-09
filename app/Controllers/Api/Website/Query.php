d27d8446e52ba37a7d96d4cbac6e70c73359d720
Pony <pony@ponycool.com>
2026-03-09 14:51:26 +0800

    Update Query.php

--- app/Controllers/Api/Website/Query.php
+++ app/Controllers/Api/Website/Query.php
@@ -23,7 +23,6 @@ class Query extends Base
     public function index(): void
     {
         $this->postFilter();
-        $this->validatePageParamsFromJsonInput();
         $svc = new WebsiteService();
         $rules = $svc->getBaseRules();
         $this->verifyJsonInputByRules($rules);
